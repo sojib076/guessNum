503af7dbc3099c76b47883630513da68c37338bc
sojib076 <sojibds332@gmail.com>
2022-06-29 22:45:01 +0600

    guessNumber

---
+++ script.js
@@ -0,0 +1,16 @@
+function guessNum(pin) {
+    const handel = document.getElementById("sojib").value
+    const random = Math.floor(Math.random() * 100)+1
+   const result=document.getElementById("result2").innerHTML =  `your random number is ` + random
+    if (handel == random) {
+        const result = document.getElementById("result").innerHTML = "You Win"
+    } else {
+        const result = document.getElementById("result").innerHTML = "You Lose"
+    }
+  
+}
+
+
+
+
+
